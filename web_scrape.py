53db603ad06d77514dc26bb023a34b40c1db4a17
Erik Andrén Zachrisson <erik@zachrisson.info>
2015-01-11 15:46:33 +0100

    Create email send procedure

--- web_scrape.py
+++ web_scrape.py
@@ -4,10 +4,19 @@
 
 import webbis
 import webbis_sql
+import smtplib
 
-mail_passwd = raw_input('Enter mail password:')
+# Send email
+def send_email(username, password):
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail("erik.andren@gmail.com", ["erik.andren@gmail.com"], "Testing")
+    server.quit()
 
-
+#mail_passwd = raw_input('Enter mail password:')
 
 sql_handle = webbis_sql.WebbisSql('webbis.db')
 
@@ -16,6 +25,8 @@ print "Stored entry is ", str(last_stored_entry)
 i = last_stored_entry
 none_cnt = 0
 max_i = 10000
+
+#send_email("erik.andren@gmail.com", mail_passwd)
 
 while True:
     i = i + 1
@@ -33,4 +44,3 @@ while True:
     sql_handle.store(newWebbis)
     sql_handle.update_last(i)
     
-# Send email
